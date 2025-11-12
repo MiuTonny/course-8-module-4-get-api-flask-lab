662bd571a2ebe045d1fb5ae080d2590c35dd4614
Milton Tavares <Miltonp.Tavares@hotmail.com>
2025-11-12 10:32:14 -0500

    Clean repo: add Flask GET API lab files and ignore venv

--- app.py
+++ app.py
@@ -7,19 +7,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    pass  # TODO: Return a welcome message
+    return jsonify(message="Welcome to the Homepage!"), 200  # TODO: Return a welcome message
 
 # TODO: Implement GET /products route that returns all products or filters by category
 
 @app.route("/products")
 def get_products():
-    pass  # TODO: Return all products or filter by ?category=
+      category = request.args.get("category")
+      if category:
+           filtered = [p for p in products if p["category"].lower() == category.lower()]
+           return jsonify(filtered), 200
+      return jsonify(products), 200# TODO: Return all products or filter by ?category=
 
 # TODO: Implement GET /products/<id> route that returns a specific product by ID or 404
 
 @app.route("/products/<int:id>")
 def get_product_by_id(id):
-    pass  # TODO: Return product by ID or 404
+    for p in products:
+         if p["id"] == id:
+              return jsonify(p), 200
+    return jsonify(error="Product not found"), 404  # TODO: Return product by ID or 404
 
 if __name__ == "__main__":
     app.run(debug=True)
